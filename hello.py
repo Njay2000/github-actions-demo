7183ae5a35a940950b13bc347a4df546199aca6d
Ratish Chinnasamy <narenhrithik2000@gmail.com>
2024-06-06 14:55:31 +0100

    updated the greeting message new eddit

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def say_hello():
-    return "Hello, GitHub Actions! Welcome to CI/CD!"
-
+    return "Hello, GitHub Actions! Welcome to CI/CD! I have made this changes, is the buils gonna start??
+"
 if __name__ == "__main__":
     print(say_hello())
